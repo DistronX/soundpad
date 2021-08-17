01ddb650b3de4f1f470b2aa26dd56f790b5c8168
Ambuj Gupta <ambujgupta2000@gmail.com>
2021-08-17 19:50:06 +0530

    Added makeSound()

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 for (var i=0; i<7; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function () {
-        buttonAnimation(this.innerHTML);
+        var currentButton = this.innerHTML;
+        buttonAnimation(currentButton);
+        makeSound(currentButton);
     });
 }
 
 document.addEventListener("keypress",function (event){
-    buttonAnimation(event.key)
-    var audio = new Audio("sounds/illuminati.mp3");
-    audio.play();
+    makeSound(event.key);
 });
+
+function makeSound(currentButton) {
+    buttonAnimation(currentButton);
+
+    switch (currentButton) {
+        case 'w':
+            var audio = new Audio("sounds/illuminati.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio("sounds/illuminati.mp3");
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio("sounds/illuminati.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio("sounds/illuminati.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio("sounds/illuminati.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio("sounds/illuminati.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio("sounds/illuminati.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
 
 function buttonAnimation(buttonId) {
     var currentButton = document.querySelector("." + buttonId)
